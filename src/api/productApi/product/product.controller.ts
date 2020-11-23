eca76a62cad4cc9e5fa222a5d4fa79aa8aa378ab
farhanfitrahtur <farhanfitrahtur@gmail.com>
2020-11-23 20:16:41 +0700

    feat(category): Sync store and update function

--- src/api/productApi/product/product.controller.ts
+++ src/api/productApi/product/product.controller.ts
@@ -13,4 +13,32 @@ export default class ProductController {
       next(new httpException(error.statusCode || 500, error.message))
     }
   }
+
+  async store(req: Request, res: Response, next: NextFunction) {
+    try {
+      const product = await productService.createProduct(req.body)
+      res.send(product)
+    } catch (error) {
+      next(new httpException(error.statusCode || 500, error.message))
+    }
+  }
+
+  async edit(req: Request, res: Response, next: NextFunction) {
+    try {
+      const data = await productService.updateProduct(req.params.id,req.body)
+      res.send(data)
+    } catch (error) {
+      next(new httpException(error.statusCode || 500, error.message))
+    }
+  }
+
+  async delete(req: Request, res: Response, next: NextFunction) {
+    try {
+      const data = await productService.deleteProduct(req.params.id)
+      res.send(data)
+    } catch (error) {
+      next(new httpException(error.statusCode || 500, error.message))
+    }
+  }
+
 }
